16ab075bc9f1b1c5f8beeff5bd27794f09c40e7c
Grillio <bggriller@gmail.com>
2023-05-09 15:49:30 -0400

    Added in scroll wheel functionality

--- Assets/Scripts/CameraMotion.cs
+++ Assets/Scripts/CameraMotion.cs
@@ -1,9 +1,23 @@
 using UnityEngine;
+using System.Collections;
 
 public class CameraMotion : MonoBehaviour
 {
+    [Header("Camera Movement")]
     [SerializeField] private float CameraSpeed = 0.0f;
 
+    [Header("Camera Height")]
+    [SerializeField] private float ScrollWheelIntensity = 2f;
+    [SerializeField] private Vector2 HeightBounds = new Vector2(3, 13);
+    [SerializeField] private float HeightAdjustSpeed = 0.0f;
+
+    private float HeightVar;
+
+    void Awake(){
+
+        HeightVar = Camera.main.fieldOfView;
+
+    }
 
     void FixedUpdate(){
 
@@ -11,5 +25,15 @@ public class CameraMotion : MonoBehaviour
         float yVar = Input.GetAxis("Vertical") * CameraSpeed * Time.deltaTime;
 
         transform.position += new Vector3(xVar, yVar);
+    }
+
+    void Update(){
+
+        HeightVar -= Input.mouseScrollDelta.y * ScrollWheelIntensity;
+        HeightVar = Mathf.Clamp(HeightVar, HeightBounds.x, HeightBounds.y);
+        float Height = Mathf.MoveTowards(Camera.main.fieldOfView, HeightVar, HeightAdjustSpeed);
+
+        Camera.main.fieldOfView = Height;
+
     }
 }
