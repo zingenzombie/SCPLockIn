dcfec9b7b74826257e3d9980ae2ef8dc3f100623
Grillio <bggriller@gmail.com>
2023-05-11 00:04:46 -0400

    Grid System v.1

---
+++ Assets/GridSystem/Scripts/TileRandomColor.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileRandomColor : MonoBehaviour
+{
+    private Color32 Green1 = new Color32(88, 171,90,255);
+    private Color Green2 = new Color32(95, 183, 97, 255);
+    void Start()
+    {
+        SpriteRenderer renderer = GetComponent<SpriteRenderer>();
+        renderer.color = Color32.Lerp(Green1, Green2, Random.Range(0.0f, 1.0f));
+    }
+}
